f5c3b6508d26e2b49bb8961e06b7bfcdadbe69d1
Hope Kenga <97263417+HopeKenga@users.noreply.github.com>
2022-03-03 14:49:44 +0300

    login

--- main.js
+++ main.js
@@ -5,4 +5,11 @@ let navbar = document.querySelector('.navbar');
 menu.onclick = () => {
     navbar.classList.toggle('active');
     menu.classList.toggle('move');
+    cart.classList.remove('active');
+}
+let cart = document.querySelector('.cart');
+
+document.querySelector('#cart-icon').onclick = () => {
+
+    cart.classList.toggle('active');
 }
