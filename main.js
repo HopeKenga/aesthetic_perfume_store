a0048f247d44dddf3e11b9cb26349248d81e3f42
Hope Kenga <97263417+HopeKenga@users.noreply.github.com>
2022-03-06 12:24:37 +0300

    home

--- main.js
+++ main.js
@@ -3,24 +3,26 @@ let menu = document.querySelector('.menu-icon');
 let navbar = document.querySelector('.navbar');
 
 menu.onclick = () => {
-    navbar.classList.toggle('active');
-    menu.classList.toggle('move')
-    cart.classList.remove('active');
-}
+        navbar.classList.toggle('active');
+        menu.classList.toggle('move')
+        cart.classList.remove('active');
+    }
+    //Cart Toggle
 let cart = document.querySelector('.cart');
 
 document.querySelector('#cart-icon').onclick = () => {
 
-    cart.classList.toggle('active');
-    navbar.classList.remove('active');
-    menu.classList.remove('move')
-}
+        cart.classList.toggle('active');
+        navbar.classList.remove('active');
+        menu.classList.remove('move')
+        login.classList.remove('active');
+    }
+    //Login-Form Toggle
 let login = document.querySelector('.login-form');
 
 document.querySelector('#user-icon').onclick = () => {
-
     login.classList.toggle('active');
-    cart.classList.toggle('active');
+    cart.classList.remove('active');
     navbar.classList.remove('active');
-    menu.classList.remove('move');
+    menu.classList.remove('move')
 }
